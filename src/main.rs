a2aaa0f481fcff3e477b1edcc6eca25264c0e931
Benjamin Hetherington <b.w.hetherington@gmail.com>
2018-01-11 00:13:00 -0500

    Moved REPL code into its own file

--- src/main.rs
+++ src/main.rs
@@ -22,66 +22,32 @@ use values::*;
 mod environment;
 use environment::Environment;
 
-use std::io::{self, BufReader, Error, Write};
+mod repl;
+use repl::*;
+
+use std::io::BufReader;
 use std::fs::File;
 
 const NAME: &'static str = env!("CARGO_PKG_NAME");
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-fn read_input_line() -> Result<String, Error> {
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf)?;
-    Ok(buf)
-}
-
-fn repl(ctx: &mut Environment) {
-    loop {
-        let prompt = format!("{}>", utils::user_name());
-        print!("{} ", Blue.bold().paint(prompt));
-        let _ = std::io::stdout().flush();
-        if let Ok(line) = read_input_line() {
-            let mut parser = Parser::new();
-            let mut reader = BufReader::new(line.as_bytes());
-            let parsed = parser.parse_all(&mut reader).unwrap_or_else(|e| {
-                let err = Red.paint(format!("ERROR: {}", e));
-                println!("{}", err);
-                vec![SExpr::Nil]
-            });
-
-            for exp in parsed.iter() {
-                match exp.eval(ctx) {
-                    Ok(res) => match res {
-                        Value::List(ref l) if l.is_empty() => continue,
-                        _ => println!("{}", res)
-                    },
-                    Err(err) => {
-                        let err = format!("ERROR: {}", err);
-                        println!("{}", Red.paint(err));
-                        break;
-                    }
-                }
-            }
-        }
-    }
-}
-
 fn main() {
     // Create context
-    let mut ctx = Environment::new();
-    ctx.init_intrinsics();
+    let mut env = Environment::new();
+    env.init_intrinsics();
 
-    // Read sample file
+    // Read library
     let _ = File::open("lisp-lib/stdlib.rkt").map(|f| {
         let mut reader = BufReader::new(f);
         let mut parser = Parser::new();
         let parsed = parser.parse_all(&mut reader)
             .unwrap_or_else(|err| {
                 let message = format!("ERROR: {}", err);
-                println!("{}", Red.paint(message));
+                println!("{}", (message));
                 vec![]
             });
         for expr in parsed.iter() {
-            let res = expr.eval(&mut ctx);
+            let res = expr.eval(&mut env);
             match res {
                 Ok(val) => match val {
                     Value::List(ref l) if l.len() == 0 => continue,
@@ -89,12 +55,12 @@ fn main() {
                 },
                 Err(why) => {
                     let message = format!("ERROR: {}", why);
-                    println!("{}", Red.paint(message));
+                    println!("{}", color::red(message));
                 }
             }
         }
     });
 
     println!("Welcome to {} v{}.", NAME, VERSION);
-    repl(&mut ctx);
+    run(&mut env);
 }
---
+++ src/repl.rs
@@ -0,0 +1,90 @@
+use std::io::{self, BufReader, Read, Error, Write};
+use std::fs::File;
+
+use environment::*;
+use parser::*;
+use utils::*;
+use sexpr::*;
+use interpreter::*;
+use values::*;
+
+pub mod color {
+    /// Does nothing to the specified string.
+    #[cfg(target_os = "windows")]
+    pub fn blue<S: Into<String>>(s: S) -> String {
+        s.into()
+    }
+
+    /// Paints the specified string blue.
+    #[cfg(not(target_os = "windows"))]
+    pub fn blue<S: Into<String>>(s: S) -> String {
+        Blue.bold().paint(s.into())
+    }
+
+    /// Does nothing to the specified string.
+    #[cfg(target_os = "windows")]
+    pub fn red<S: Into<String>>(s: S) -> String {
+        s.into()
+    }
+
+    /// Paints the specified string red.
+    #[cfg(not(target_os = "windows"))]
+    pub fn red<S: Into<String>>(s: S) -> String {
+        Red.paint(s.into())
+    }
+}
+
+fn read_input_line() -> Result<String, Error> {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    Ok(buf)
+}
+
+fn parse_line<S: AsRef<str>>(line: S) -> Result<Vec<SExpr>, String> {
+    let mut parser = Parser::new();
+    let bytes = line.as_ref().as_bytes();
+    let mut reader = BufReader::new(bytes);
+    
+    parser.parse_all(&mut reader)
+}
+
+pub fn print_prompt<S: AsRef<str>>(prompt: S) -> io::Result<()> {
+    print!("{}", prompt.as_ref());
+    io::stdout().flush()
+}
+
+pub fn print_err<S: AsRef<str>>(why: S) {
+    let err = color::red(format!("ERROR: {}", why.as_ref()));
+    println!("{}", err);
+}
+
+/// Evaluates the specified expressions. 
+fn eval_exprs(env: &mut Environment, exprs: &[SExpr]) {
+    for expr in exprs {
+        match expr.eval(env) {
+            Ok(res) => match res {
+                Value::List(ref vals) if vals.is_empty() => continue,
+                _ => println!("{}", res)
+            },
+            Err(why) => {
+                print_err(why)
+            }
+        }
+    }
+}
+
+/// Runs a REPL for the specified environment.
+pub fn run(env: &mut Environment) {
+    let prompt = color::blue(format!("{}> ", user_name()));
+    loop {
+        print_prompt(&prompt).expect("Failed to print prompt.");
+        if let Ok(line) = read_input_line() {
+            match parse_line(line) {
+                Ok(exprs) => eval_exprs(env, &exprs),
+                Err(why) => print_err(why)
+            }
+        } else {
+            print_err("Could not read input.");
+        }
+    }
+}
